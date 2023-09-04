cfb57c2036b461d6cbddac35faacf52533fc8f2b
Felipr19 <142014516+Felipr19@users.noreply.github.com>
2023-09-04 06:48:17 -0500

    Create tienda.py

---
+++ Ejercicio4/tienda.py
@@ -0,0 +1,13 @@
+from animales import *
+from random import choice
+
+class Tienda:
+    
+    def __init__(self) -> None:
+        self.animales=[]
+
+    def agregar_animal(self,Animal):
+        self.animales.append(Animal)
+
+    def entregar_animal(self):
+        return(choice(self.animales))
